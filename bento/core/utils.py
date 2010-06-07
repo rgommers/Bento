626ccb1772f0528f07da57df30ba828c430a7273
David Cournapeau <cournape@gmail.com>
2010-06-07 16:58:07 +0900

    ENH: add cpu_count function (from multiprocessing).

--- bento/core/utils.py
+++ bento/core/utils.py
@@ -183,3 +183,27 @@ def ensure_directory(path):
     d = os.path.dirname(path)
     if d and not os.path.exists(d):
         os.makedirs(d)
+
+# Shamelessly copied from multiprocessing
+def cpu_count():
+    """Returns the number of CPUs in the system"""
+    if sys.platform == 'win32':
+        try:
+            num = int(os.environ['NUMBER_OF_PROCESSORS'])
+        except (ValueError, KeyError):
+            num = 0
+    elif 'bsd' in sys.platform or sys.platform == 'darwin':
+        try:
+            num = int(os.popen('sysctl -n hw.ncpu').read())
+        except ValueError:
+            num = 0
+    else:
+        try:
+            num = os.sysconf('SC_NPROCESSORS_ONLN')
+        except (ValueError, OSError, AttributeError):
+            num = 0
+
+    if num >= 1:
+        return num
+    else:
+        raise NotImplementedError('cannot determine number of cpus')
